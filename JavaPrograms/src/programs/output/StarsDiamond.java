305a2f7b578ea2ec73572a3f76fbc836f19918d7
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-05 01:54:35 +0200

    Create StarsDiamond.java

---
+++ JavaPrograms/src/programs/output/StarsDiamond.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programs.output;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class StarsDiamond {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // Upper part of the diamond
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

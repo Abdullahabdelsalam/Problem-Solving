857c66308b2c10b52b6ed3dfe5fec035df8ba827
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-05 05:34:50 +0200

    Create NestedIfElse.java

---
+++ JavaPrograms/src/hundredofprograms/partone/NestedIfElse.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hundredofprograms.partone;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class NestedIfElse {
+
+    public static void main(String[] args) {
+        int marksObtained, passingMarks;
+        char grade;
+
+        passingMarks = 40;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input marks scored by you = ");
+
+        marksObtained = in.nextInt();
+
+        if (marksObtained >= passingMarks) {
+
+            if (marksObtained > 90) {
+                grade = 'A';
+            } else if (marksObtained > 75) {
+                grade = 'B';
+            } else if (marksObtained > 60) {
+                grade = 'C';
+            } else {
+                grade = 'D';
+            }
+
+            System.out.println("You passed the exam and your grade is " + grade);
+        } else {
+            grade = 'F';
+            System.out.println("You failed and your grade is " + grade);
+        }
+        in.close();
+    }
+
+}

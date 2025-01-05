f65bd790e58d14a8eca800185f39e4536120e3bc
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-05 05:35:20 +0200

    Create WhileLoop.java

---
+++ JavaPrograms/src/hundredofprograms/partone/WhileLoop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hundredofprograms.partone;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1; 
+
+        while (i < 6) {
+            System.out.println(i);
+            i++; // Increment the counter
+        }
+
+    }
+}

7f5894b5efd275debd5cb82d1350732614258924
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-12 17:22:19 +0200

    Update ReverseString.java

--- JavaPrograms/src/hundredofprograms/parttwo/ReverseString.java
+++ JavaPrograms/src/hundredofprograms/parttwo/ReverseString.java
@@ -18,10 +18,11 @@ public class ReverseString {
         System.out.println("Enter a string to reverse");
         original = in.nextLine();
         int length = original.length();
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = length - 1; i > -1; i--) {
             reverse = reverse + original.charAt(i);
         }
         System.out.println ("Reverse of entered string is: "+reverse); 
+        in.close();
     }
 
     

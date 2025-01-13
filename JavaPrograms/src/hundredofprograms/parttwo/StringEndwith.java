bc84ee26f059a647fd8c4a50aa5799607f3dc696
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-13 17:07:18 +0200

    Create StringEndwith.java

---
+++ JavaPrograms/src/hundredofprograms/parttwo/StringEndwith.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hundredofprograms.parttwo;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class StringEndwith {
+
+    public static void main(String args[]) {
+        String s1 = "java by TechnoLamror";
+        System.out.println(s1.endsWith("r"));
+        System.out.println(s1.endsWith("Lamror"));
+        System.out.println(s1.endsWith("lamror"));
+    }
+
+}

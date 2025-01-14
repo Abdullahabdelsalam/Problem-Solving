d9530f48b00141cd59c5eec22fc4e811a6121c4c
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-14 21:31:18 +0200

    Create ReplaceString.java

---
+++ JavaPrograms/src/hundredofprograms/parttwo/ReplaceString.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hundredofprograms.parttwo;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class ReplaceString {
+
+    public static void main(String[] args) {
+
+        String s1 = "My name is Rajendra. My name is lamror. My name is Technolamror.";
+        String replaceString = s1.replaceAll("is", "was");
+        System.out.println(s1);
+        System.out.println(replaceString);
+    }
+
+}

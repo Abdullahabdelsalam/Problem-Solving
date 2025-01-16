4a572ac2862fda049089afdf208be40e68229808
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:03:55 +0200

    Create CompareString.java

---
+++ JavaPrograms/src/programsSE/string/CompareString.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.string;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class CompareString {
+
+    public static void main(String args[]) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "meklo";
+        String s4 = "hemlo";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}

1430323eb0ad7180f754d514b89761693f576230
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:04:05 +0200

    Create Palindrome.java

---
+++ JavaPrograms/src/programsSE/string/Palindrome.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.string;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class Palindrome {
+
+    public static void main(String args[]) {
+        String original, reverse = "";
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a string to check if it is a palindrome :");
+        original = in.nextLine();
+
+        int length = original.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + original.charAt(i);
+        }
+
+        if (original.equals(reverse)) {
+            System.out.println(" is a palindrome.");
+        } else {
+            System.out.println("is not a palindrome.");
+        }
+        in.close();
+    }
+
+}

d2192e924deabfe8a70fbb3f29ea5f5622e7c095
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:03:37 +0200

    Create Constructor.java

---
+++ JavaPrograms/src/programsSE/methods/constructor/Constructor.java
@@ -0,0 +1,11 @@
+package programsSE.methods.constructor;
+
+public class Constructor {
+    //constructor method
+    Constructor() {
+        System.out.println("Constructor method called.");
+    }
+    public static void main(String[] args) {
+        Constructor object = new Constructor(); //creating object
+    }
+}

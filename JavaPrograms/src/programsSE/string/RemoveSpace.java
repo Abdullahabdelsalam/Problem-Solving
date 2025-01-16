49aa9226520e50f4aa306f141fb6c56917a2331c
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:04:10 +0200

    Create RemoveSpace.java

---
+++ JavaPrograms/src/programsSE/string/RemoveSpace.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.string;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class RemoveSpace {
+
+    public static void main(String args[]) {
+        String s1 = "  hello string   ";
+        System.out.println(s1 + "Technolamror");
+        System.out.println(s1.trim() + "Technolamror");
+    }
+}

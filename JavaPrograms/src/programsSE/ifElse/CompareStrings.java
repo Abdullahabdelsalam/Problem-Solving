a9c9326f129f338274b89f67fa3da717fcdb4507
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:06:13 +0200

    Create CompareStrings.java

---
+++ JavaPrograms/src/programsSE/ifElse/CompareStrings.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.ifElse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class CompareStrings {
+
+    public static void main(String args[]) {
+        String s1, s2;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the first string");
+        s1 = in.nextLine();
+        System.out.println("Enter the second string");
+        s2 = in.nextLine();
+
+        if (s1.compareTo(s2) > 0) {
+            System.out.println("First string is greater than second.");
+        } else if (s1.compareTo(s2) < 0) {
+            System.out.println("First string is smaller than second.");
+        } else {
+            System.out.println("Both strings are equal.");
+        }
+    }
+
+}

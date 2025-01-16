358029233213d12994c9f9bcd3a48bb08dab2904
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:06:41 +0200

    Create SwapNumbers.java

---
+++ JavaPrograms/src/programsSE/datatypes/SwapNumbers.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.datatypes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class SwapNumbers {
+   
+    public static void main(String[] args) {
+        
+        int numb1,numb2,numb3=0 ;
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter the numb1:");
+        numb1 = in.nextInt();
+        
+        System.out.println("Enter the numb2:");
+        numb2 = in.nextInt();
+        
+        numb3 = numb1;
+        numb1 = numb2;
+        numb2 = numb3;
+        
+        System.out.println("After swap numb1 = " + numb1);
+        
+        System.out.println("After swap numb2 = " + numb2);
+        
+        in.close();
+        
+    }
+}

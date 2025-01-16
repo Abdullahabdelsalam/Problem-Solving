4ea5dcbb559937937748951f6ab1db1ce5e81e4c
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:03:47 +0200

    Create DifStaticInstance.java

---
+++ JavaPrograms/src/programsSE/methods/DifStaticInstance.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programsSE.methods;
+
+/**
+ *
+ * @author Abdullah
+ */
+public class DifStaticInstance {
+
+    public static void main(String[] args) {
+        display();  //calling without object 
+        DifStaticInstance t = new DifStaticInstance();
+        t.show();  //calling using object 
+    }
+
+    static void display() {
+        System.out.println("Programming is amazing.");
+    }
+
+    void show() {
+        System.out.println("Java is awesome.");
+    }
+
+}

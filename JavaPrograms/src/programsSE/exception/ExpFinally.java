965ead8818cbdb290b5560270ef6c5f0c1f58a0d
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-16 21:06:30 +0200

    Create ExpFinally.java

---
+++ JavaPrograms/src/programsSE/exception/ExpFinally.java
@@ -0,0 +1,17 @@
+package programsSE.exception;
+
+public class ExpFinally {
+    public static void main(String[] args) {
+
+        try {
+            long data[] = new long[1000000000];
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        finally {
+            System.out.println("finally block will execute always.");
+        }
+    }
+}

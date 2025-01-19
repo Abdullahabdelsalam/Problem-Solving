e9d0811b22e9b39a2c1076817ec9b570797ca0e7
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-19 09:56:36 +0200

    Update Notepad.java

--- JavaPrograms/src/programsSE/datatypes/Notepad.java
+++ JavaPrograms/src/programsSE/datatypes/Notepad.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 public class Notepad {
     public static void main(String[] args) {
-        Runtime rs = Runtime.getRuntime();
+        Runtime runtime  = Runtime.getRuntime();
 
         try {
-            rs.exec("notepad");
+            runtime .exec("notepad");
         }
         catch (IOException e) {
             System.out.println(e);

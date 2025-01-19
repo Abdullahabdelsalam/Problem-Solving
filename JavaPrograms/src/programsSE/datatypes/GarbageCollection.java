2b93f80781bcc934a46ed2f7a8cdd0705169a4f2
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-19 09:56:42 +0200

    Update GarbageCollection.java

--- JavaPrograms/src/programsSE/datatypes/GarbageCollection.java
+++ JavaPrograms/src/programsSE/datatypes/GarbageCollection.java
@@ -3,9 +3,9 @@ package programsSE.datatypes;
 public class GarbageCollection {
     public static void main(String s[]) throws Exception
     {
-        Runtime rs =  Runtime.getRuntime();
-        System.out.println("Free memory in JVM before Garbage Collection = "+rs.freeMemory());
-                rs.gc();
-        System.out.println("Free memory in JVM after Garbage Collection = "+rs.freeMemory());
+        Runtime runtime  =  Runtime.getRuntime();
+        System.out.println("Free memory in JVM before Garbage Collection = "+runtime .freeMemory());
+                runtime .gc();
+        System.out.println("Free memory in JVM after Garbage Collection = "+runtime .freeMemory());
     }
 }

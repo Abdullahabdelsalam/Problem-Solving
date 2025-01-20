29122c6ed569cb4d90389d150269833b93827560
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-20 21:33:10 +0200

    Create BinarySearch.class

--- JavaPrograms/src/programsSE/file/FileOutputStreamExa.java
+++ JavaPrograms/src/programsSE/file/FileOutputStreamExa.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 public class FileOutputStreamExa {
     public static void main(String args[]){
         try{
-            FileOutputStream fout=new FileOutputStream("D:\\testout.txt");
+            FileOutputStream fout = new FileOutputStream("D:\\testout.txt");
             fout.write(65);
             fout.close();
             System.out.println("success.. by Technolamror.");

569f7d82a9425ec3c8c2361e3b1b195a76fb0bab
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-21 09:22:26 +0200

    Delete StringEndwith.class

---
+++ JavaPrograms/src/programsSE/convert/StringToInt.java
@@ -0,0 +1,10 @@
+package programsSE.convert;
+
+public class StringToInt {
+    public static void main(String args[]){
+        String s="200";
+        int i=Integer.parseInt(s);
+        System.out.println(s+100);
+        System.out.println(i+100);
+    }
+}

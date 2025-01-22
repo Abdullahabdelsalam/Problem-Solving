6759ff25e941196af563e33f2f9c26420a0482f1
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-22 10:52:00 +0200

    Create StringToFloatEx.java

---
+++ JavaPrograms/src/programsSE/convert/StringToFloatEx.java
@@ -0,0 +1,9 @@
+package programsSE.convert;
+
+public class StringToFloatEx {
+    public static void main(String args[]){
+        String s = "23.6";
+        float f = Float.parseFloat("23.6");
+        System.out.println(f);
+    }
+}

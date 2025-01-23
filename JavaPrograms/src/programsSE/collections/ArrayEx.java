3767f98b3341d2a77bf2063346c4eff5494c74d9
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-23 14:36:50 +0200

    Create ArrayEx.java

---
+++ JavaPrograms/src/programsSE/collections/ArrayEx.java
@@ -0,0 +1,17 @@
+package programsSE.collections;
+
+public class ArrayEx {
+    public static void main(String args[]){
+
+        int a[]=new int[5];
+        a[0]=10;//initialization
+        a[1]=20;
+        a[2]=70;
+        a[3]=40;
+        a[4]=50;
+
+        for(int i=0;i<a.length;i++)
+            System.out.println(a[i]);
+
+    }
+}

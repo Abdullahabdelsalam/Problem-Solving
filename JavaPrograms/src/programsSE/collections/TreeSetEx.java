74f8d87da72500d105a03221e357b79dbafd1eb7
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-23 14:37:22 +0200

    Create TreeSetEx.java

---
+++ JavaPrograms/src/programsSE/collections/TreeSetEx.java
@@ -0,0 +1,19 @@
+package programsSE.collections;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeSetEx {
+    public static void main(String args[]){
+//Creating and adding elements
+        TreeSet<String> al=new TreeSet<String>();
+        al.add("Rajendra");
+        al.add("Raja");
+        al.add("Ravi");
+        al.add("Technolamror");
+        Iterator<String> itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}

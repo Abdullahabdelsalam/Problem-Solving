af35a50629487a93af53b560804208c1ae5ba783
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-23 14:37:05 +0200

    Create LinkedHashMapEx.java

---
+++ JavaPrograms/src/programsSE/collections/LinkedHashMapEx.java
@@ -0,0 +1,19 @@
+package programsSE.collections;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapEx {
+    public static void main(String args[]){
+
+        LinkedHashMap<Integer,String> hm=new LinkedHashMap<Integer,String>();
+
+        hm.put(100,"Rajendra");
+        hm.put(101,"Vijay");
+        hm.put(102,"Technolamror");
+
+        for(Map.Entry m:hm.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+    }
+}
